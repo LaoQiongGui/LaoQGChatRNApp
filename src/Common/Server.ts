fedb3be6611b5bf38fc22036c5f56de4f8315417
LaoQiongGui <2307712195@qq.com>
2024-07-30 02:13:32 +0800

    修改：host修正

--- src/Common/Server.ts
+++ src/Common/Server.ts
@@ -8,5 +8,5 @@ export class Server {
     }
 }
 
-export const myServer = new Server('10.0.2.2', 12195);
-// export const myServer = new Server('101.43.102.43', 12195);
+// export const myServer = new Server('10.0.2.2', 12195);
+export const myServer = new Server('101.43.102.43', 12195);
